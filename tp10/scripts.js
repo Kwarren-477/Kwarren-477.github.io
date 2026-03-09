d4d174029cc3690afddd2ab0c6a7ce399486ebd3
Kwarren-477 <kayla.l.warren@wsu.edu>
2026-03-09 02:01:54 +0000

    added clickable code

--- tp10/scripts.js
+++ tp10/scripts.js
@@ -1,139 +1,197 @@
 // declare the board data for a game, using 3 arrays
 // "-" indicates unmarked, "x" indicates an X mark, "o" indicates an O mark
-let rowA = [ "-", "-", "-" ];
-let rowB = [ "-", "-", "-" ];
-let rowC = [ "-", "-", "-" ];
+let rowA = ["-", "-", "-"];
+let rowB = ["-", "-", "-"];
+let rowC = ["-", "-", "-"];
 
 
+//*track who turn it is*//
 
-function compareSpaces (a, b, c,) {
-  return ( (a==b) && (a==c) );
+let currentTurn = "x";
+
+//set up blank varibale for player//
+let currentPlayer;
+
+//*comparing gameboard sections*//
+function compareSpaces(a, b, c,) {
+  return ((a == b) && (a == c));
 }
+
+
+  //function handle click//
+
+  function clickSquare() {
+
+    //check space//
+    if (this.innerHTML == "") {
+      //set space
+      this.innerHTML = currentTurn;
+
+      //flip turns//
+
+      if (currentTurn == "x") currentTurn = "o";
+      else currentTurn = "x";
+      
+      //update player dom//
+      currentPlayer.innerHTML=currentTurn;
+
+    }
+
+  }
+/*code for the docmuent to finish loading before game can start*//
+document.addEventListener("DOMContentLoaded", function () {
+
+  //*All clickable spaces*//
+  let allSpaces = document.querySelectorAll(".gamespace");
+
+
+  //*beginning loop code*//
+
+  for (x = 0; x < allSpaces.length; x++) {
+    allSpaces[x].addEventListener("click", clickSquare);
+  }
+
+  //*loop with "for-of" through all clickable spaces*//
+
+  for (let eachSpace of allSpaces) {
+    eachSpace.addEventListener("click", function () {
+      this.innerHTML = "x";
+    })
+  }
+//current player Dom element//
+currentPlayer = document.querySelector("currentPlayer span");
+currentPlayer.innerHTML=currentTurn;
+
+});
+
+
+
 
 
 function checkGameboard(checkA, checkB, checkC) {
- let resultValue= "d";
-  
-  if  ( compareSpaces ( checkA[0], checkB[0], checkC[0] )  ) {
-    
+  let resultValue = "d";
+
+  if (compareSpaces(checkA[0], checkB[0], checkC[0])) {
+
   }
   //check for vertical rows path//
   // row 0 test//
-  if ( compareSpaces ( checkA[0], checkB[0], checkC[0] )  ) {
-   if (checkA[0]=="x") resultValue="x";
-   else if (checkA[0]="o") resultValue ="o";
-}
+  if (compareSpaces(checkA[0], checkB[0], checkC[0])) {
+    if (checkA[0] == "x") resultValue = "x";
+    else if (checkA[0] = "o") resultValue = "o";
+  }
   //row 1 test//
-    if ( compareSpaces ( checkA[1], checkB[1], checkC[1] )  ) {
-   if (checkA[1]=="x") resultValue="x";
-   else if (checkA[1]="o") resultValue ="o";
-}
-//row 2 test//
-     if ( compareSpaces ( checkA[2], checkB[2], checkC[2] )  ) {
-   if (checkA[2]=="x") resultValue="x";
-   else if (checkA[2]="o") resultValue ="o";
-}
-  
+  if (compareSpaces(checkA[1], checkB[1], checkC[1])) {
+    if (checkA[1] == "x") resultValue = "x";
+    else if (checkA[1] = "o") resultValue = "o";
+  }
+  //row 2 test//
+  if (compareSpaces(checkA[2], checkB[2], checkC[2])) {
+    if (checkA[2] == "x") resultValue = "x";
+    else if (checkA[2] = "o") resultValue = "o";
+  }
 
-  
+
+
   //test for horizontal rows//
-  
- // row A test//
-  function compareSpaces (a,b,c) {
-  return ( (a==b) && (b==c) );
-}
 
-  
-  
- if  ( compareSpaces ( checkA[0], checkA[1], checkA[2] )  ) {
-    
+  // row A test//
+  function compareSpaces(a, b, c) {
+    return ((a == b) && (b == c));
   }
-  
-  if ( compareSpaces ( checkA[0], checkA[1], checkA[2] )  ) {
-   if (checkA[0]=="x") resultValue="x";
-   else if (checkA[0]="o") resultValue ="o";
-}
 
 
- 
+
+  if (compareSpaces(checkA[0], checkA[1], checkA[2])) {
+
+  }
+
+  if (compareSpaces(checkA[0], checkA[1], checkA[2])) {
+    if (checkA[0] == "x") resultValue = "x";
+    else if (checkA[0] = "o") resultValue = "o";
+  }
+
+
+
   //row B test//
-  
 
-  
- if  ( compareSpaces ( checkB[0], checkB[1], checkB[2] )  ) {
-    
+
+
+  if (compareSpaces(checkB[0], checkB[1], checkB[2])) {
+
   }
-  
-  if ( compareSpaces ( checkB[0], checkB[1], checkB[2] )  ) {
-   if (checkB[0]=="x") resultValue="x";
-   else if (checkB[0]="o") resultValue ="o";
-}
+
+  if (compareSpaces(checkB[0], checkB[1], checkB[2])) {
+    if (checkB[0] == "x") resultValue = "x";
+    else if (checkB[0] = "o") resultValue = "o";
+  }
 
 
-  
+
   //row C test//
 
 
-  
- if  ( compareSpaces ( checkC[0], checkC[1], checkC[2] )  ) {
-    
+
+  if (compareSpaces(checkC[0], checkC[1], checkC[2])) {
+
   }
-  
-  if ( compareSpaces ( checkC[0], checkC[1], checkC[2] )  ) {
-   if (checkC[0]=="x") resultValue="x";
-   else if (checkC[0]="o") resultValue ="o";
-}
+
+  if (compareSpaces(checkC[0], checkC[1], checkC[2])) {
+    if (checkC[0] == "x") resultValue = "x";
+    else if (checkC[0] = "o") resultValue = "o";
+  }
 
 
-//check for digional rows//
- 
-// combo A0,B1,C2//
-  
-if  ( compareSpaces ( checkA[0], checkB[1], checkC[2] )  ) {
-    
-  }
-  
-  if ( compareSpaces ( checkA[0], checkB[1], checkC[2] )  ) {
-   if (checkA[0]=="x") resultValue="x";
-   else if (checkA[0]="o") resultValue ="o";
-}
+  //check for digional rows//
 
-//combo C0,B1,A2//  
-  if  ( compareSpaces ( checkC[0], checkB[1], checkA[2] )  ) {
-    
+  // combo A0,B1,C2//
+
+  if (compareSpaces(checkA[0], checkB[1], checkC[2])) {
+
   }
-  
-  if ( compareSpaces ( checkC[0], checkB[1], checkA[2] )  ) {
-   if (checkC[0]=="x") resultValue="x";
-   else if (checkC[0]="o") resultValue ="o";
-}
-  
- return resultValue; 
+
+  if (compareSpaces(checkA[0], checkB[1], checkC[2])) {
+    if (checkA[0] == "x") resultValue = "x";
+    else if (checkA[0] = "o") resultValue = "o";
+  }
+
+  //combo C0,B1,A2//  
+  if (compareSpaces(checkC[0], checkB[1], checkA[2])) {
+
+  }
+
+  if (compareSpaces(checkC[0], checkB[1], checkA[2])) {
+    if (checkC[0] == "x") resultValue = "x";
+    else if (checkC[0] = "o") resultValue = "o";
+  }
+
+  return resultValue;
 }
 
 // **********************************************
 // ***** DO NOT EDIT THE CODE BELOW THIS LINE
 // **********************************************
 
-
+/*commet out non usable code
 // get a handle on the DOM element to be updated with the outcome
 let gameOutputMsg = document.querySelector("#gameResult span");
 
 
 // call your function checkGameboard() with the 3 rows
-let winState = 
-    checkGameboard(rowA, rowB, rowC);
+let winState =
+  checkGameboard(rowA, rowB, rowC);
 
 // test the returned value of the function
-if (winState == "x") { 
+if (winState == "x") {
   gameOutputMsg.innerHTML = "X wins";
-  
+
 } else if (winState == "o") {
   gameOutputMsg.innerHTML = "O wins";
-  
+
 } else if (winState == "d") {
   gameOutputMsg.innerHTML = "draw";
-  
+
 } else {
   gameOutputMsg.innerHTML = "unknown";
 }
+*/
