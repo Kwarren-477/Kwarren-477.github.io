33fa6580b25024129f31681e0a62835ffe744b64
Kwarren-477 <kayla.l.warren@wsu.edu>
2026-05-05 22:37:40 +0000

    added links and background image

--- tp12/scripts.js
+++ tp12/scripts.js
@@ -5,12 +5,12 @@
 
 const weatherurl = 'https://weatherapi-com.p.rapidapi.com/forecast.json?days=3&q=';
 const weathetoptions = {
-	method: 'GET',
-	headers: {
-		'x-rapidapi-key': 'be6a03c956mshaee959d6d0ade22p1fb573jsna6b7c2d29696',
-		'x-rapidapi-host': 'weatherapi-com.p.rapidapi.com',
-		'Content-Type': 'application/json'
-	}
+    method: 'GET',
+    headers: {
+        'x-rapidapi-key': 'be6a03c956mshaee959d6d0ade22p1fb573jsna6b7c2d29696',
+        'x-rapidapi-host': 'weatherapi-com.p.rapidapi.com',
+        'Content-Type': 'application/json'
+    }
 };
 
 let scrollingBox;
@@ -48,12 +48,12 @@ function updateWeather(weatherObject) {
     let windspeed = weatherObject.current.wind_mph;
     let winddirection = weatherObject.current.wind_dir;
     document.querySelector("#currentWind").innerHTML = windspeed + "mph " + winddirection;
-//update future temp//
+    //update future temp//
     let futureDays = document.querySelectorAll(".futureDay");
-   
+
     for (i = 0; i < futureDays.length; i++) {
         futureDays[i].querySelector(".futureTemp span").innerHTML = weatherObject.forecast.forecastday[i].day.maxtemp_f;
-   
+
         //update windspeed//
         windspeed = weatherObject.forecast.forecastday[i].day.maxwind_mph;
         futureDays[i].querySelector(".futureWind").innerHTML = windspeed + "mph";
@@ -67,52 +67,52 @@ let ipLookupURL = "https://api.ipify.org/?format=json";
 let ipLookupOptions = {};
 
 //use Ajax for data collection//
-getData(ipLookupURL, ipLookupOptions).then(function(result){
+getData(ipLookupURL, ipLookupOptions).then(function (result) {
 
     //adding proper ip number to weather URL//
 
-     let weatherLookupURL = weatherurl + result.ip;
-     console.log(weatherLookupURL);
+    let weatherLookupURL = weatherurl + result.ip;
+    console.log(weatherLookupURL);
 
     //use resulting ip number to look up data//
-    getData(weatherLookupURL, weathetoptions).then(function(weatherResult){
+    getData(weatherLookupURL, weathetoptions).then(function (weatherResult) {
         console.log(weatherResult);
         updateWeather(weatherResult);
 
-});
-
-
-
-
-
-
-
-///Hardware for DOM loading//
-document.addEventListener("DOMContentLoaded", function () {
-    scrollingBox = document.querySelector("#futureInfo");
-    isMoving = false;
-
-    scrollingBox.addEventListener("mousedown", function (e) {
-        scrollLeftStart = scrollingBox.scrollLeft;
-        offsetLeftStart = e.pageX - scrollingBox.offsetLeft;
-        isMoving = true;
     });
 
-    scrollingBox.addEventListener("mouseleave", function (e) {
+
+
+
+
+
+
+    ///Hardware for DOM loading//
+    document.addEventListener("DOMContentLoaded", function () {
+        scrollingBox = document.querySelector("#futureInfo");
         isMoving = false;
-    });
 
-    scrollingBox.addEventListener("mouseup", function (e) {
-        isMoving = false;
-    });
+        scrollingBox.addEventListener("mousedown", function (e) {
+            scrollLeftStart = scrollingBox.scrollLeft;
+            offsetLeftStart = e.pageX - scrollingBox.offsetLeft;
+            isMoving = true;
+        });
 
-    scrollingBox.addEventListener("mousemove", function (e) {
-        e.preventDefault();
-        if (!isMoving) return;
-        scrollingBox.scrollLeft = scrollLeftStart - (e.pageX - offsetLeftStart - scrollingBox.offsetLeft);
-    });
+        scrollingBox.addEventListener("mouseleave", function (e) {
+            isMoving = false;
+        });
 
-    
+        scrollingBox.addEventListener("mouseup", function (e) {
+            isMoving = false;
+        });
+
+        scrollingBox.addEventListener("mousemove", function (e) {
+            e.preventDefault();
+            if (!isMoving) return;
+            scrollingBox.scrollLeft = scrollLeftStart - (e.pageX - offsetLeftStart - scrollingBox.offsetLeft);
+        });
+
+
 
     });
 
